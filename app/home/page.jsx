be5d0205625b0c23d19cdd64ba70c0fc97843dea
Deepanshu sharma <chuiwaldeepanshu@gmail.com>
2025-05-04 18:41:16 +0530

    maded a to do app

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -7,7 +7,26 @@ function Homes() {
         <h1>Your Tasks</h1>
         <h5>Manage your tasks and track your progress</h5>
       </div>
-      <div className="progressionbar">
+      <div className="progressionbarinfo">
+        <div className="progress">
+          <div className="pro"><p>Progress</p></div>
+          <div className="precentdisplay"><img src="percent.png" alt="" /><p>89%</p></div>
+
+        </div>
+        <div className="probar">
+          <div className="probarfill">
+
+          </div>
+        </div>
+        <div className="displaycompletedtask">
+          <div className="taskdone">
+            <p>8/16 Tasks Completed</p>
+          </div>
+          <div className="remaining">
+            <p>8 remaining</p>
+          </div>
+        </div>
+
         
       </div>
     </div>
