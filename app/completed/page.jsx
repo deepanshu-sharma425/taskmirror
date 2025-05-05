d75cb1b4266c91f7b26880b57163755541c8e749
Deepanshu sharma <chuiwaldeepanshu@gmail.com>
2025-05-05 06:41:35 +0530

    ff

--- app/completed/page.jsx
+++ app/completed/page.jsx
@@ -3,26 +3,23 @@ import React, { useEffect, useState } from 'react'
 
 function Completed() {
   const [completedTasks, setCompletedTasks] = useState([])
-  function Comp({id,text,time}){
-    return(
-      <div key={id}>
-        <div  className="completeddivmain">
-          <div className="completedtaskname">
-            <p>{text}</p>
-          </div>
-          <div className="timeforcompletdtask">
-            <img src="timer.png" alt="" />
-            <p>{time} Minutes</p>
-          </div>
-          <div className="progressionbar22">
-            <div className="progressionbarfill"></div>
-          </div>
+
+  function Comp({ text, time }) {
+    const mins = Math.floor(time) 
+    return (
+      <div className="completeddivmain">
+        <div className="completedtaskname">
+          <p>{text}</p>
         </div>
-
-
+        <div className="timeforcompletdtask">
+          <img src="timer.png" alt="timer" />
+          <p>{mins} Minutes</p>
+        </div>
+        <div className="progressionbar22">
+          <div className="progressionbarfill" style={{ width: '100%' }}></div>
+        </div>
       </div>
     )
-
   }
 
   useEffect(() => {
@@ -35,17 +32,17 @@ function Completed() {
   }, [])
 
   return (
-    <div className="homes1">
+    <div className="homes2">
       <div className="namecomple">
         <h1>Completed Tasks</h1>
         <h5>You've completed {completedTasks.length} task{completedTasks.length !== 1 && 's'}.</h5>
       </div>
-      
-      {completedTasks.map((task)=>(
-        <Comp id={task.id} text={task.text} time={task.time}/>
-      ))}
 
-      
+      {completedTasks.map(task => (
+        <div key={task.id}>
+          <Comp text={task.text} time={task.time} />
+        </div>
+      ))}
     </div>
   )
 }
